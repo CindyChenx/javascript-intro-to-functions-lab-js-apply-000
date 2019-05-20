9beac44b70abc8633babf03739b8987a95d53d44
outstanding-bolt-9487 <chen_cx0128@hotmail.com>
2019-05-20 22:04:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function logWhisper(String){
 function sayHiToGrandma(String){
   if(String.toLowerCase() === String){
     console.log("I can\'t hear you")
-  } else
+  } 
   if(String.toUpperCase() === String){
     console.log("YES INDEED!")
-  } else
+  } 
   if(String === "I love you, Grandma."){
     console.log("I love you, too.")
   }
